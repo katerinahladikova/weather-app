e1557f5ebb69f7ece83bd0b1a2e4dc51dc44b684
katerinahladikova <kat.hladikova@email.cz>
2025-07-08 19:03:44 +0200

    jsem cooked chate :(

--- script.js
+++ script.js
@@ -4,46 +4,45 @@ const searchInput = document.getElementById("search")
 const searchList = document.getElementById("search-list")
 const selectedCity = document.getElementById("selected-city")
 const dayTemperature = document.getElementById("day-temperature")
-let currentCity = null
-let cities = []
+const cities = await getCities()
 
+async function getCities() {
+    const response = await fetch("/city.list.json")
+    const data = await response.json()
+    return data
+}
 
-fetch("/city.list.json")
-.then (request => request.json())
-.then (data => cities = data)
-
-searchInput.addEventListener("keypress", function() {
-    const citiesFilter = 
-        cities.filter(city => {
-            return city.name.toLowerCase().includes(searchInput.value.toLowerCase())
-        })
-    
-    searchList.innerHTML = ""
-    citiesFilter.slice(0, 10).forEach(city => {
-        const cityEl = document.createElement("div")
-        const countryEl = document.createElement("span")
-        searchList.appendChild(cityEl)
-        cityEl.textContent = city.name
-        countryEl.textContent = city.country
-        cityEl.appendChild(countryEl)
-
-        cityEl.addEventListener("click", function(){
-            currentCity = city
-            selectedCity.textContent = city.name + ", " + city.country
-            getWeather()
-        })
-
+function filterCities(cities, value){
+    return cities.filter(city => {
+        return city.name.toLowerCase().includes(value.toLowerCase())
     })
-})
-
-function getWeather() {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${currentCity.coord.lat}&lon=${currentCity.coord.lon}&appid=${apiKey}&units=metric`)
-    .then (request => request.json())
-    .then (function(data) {
-        
-    })
-
 
 }
 
+function renderCity(city){
+    const cityEl = document.createElement("div")
+    const countryEl = document.createElement("span")
+    searchList.appendChild(cityEl)
+    cityEl.textContent = city.name
+    countryEl.textContent = city.country
+    cityEl.appendChild(countryEl)
+    
+    cityEl.addEventListener("click", function(){
+        selectedCity.textContent = city.name + ", " + city.country
+        getWeather(city)
+    })
+}
 
+function search(){
+    const citiesFilter = filterCities(cities, searchInput.value)
+    searchList.innerHTML = ""
+    citiesFilter.slice(0, 10).forEach(city => renderCity(city))
+}
+
+searchInput.addEventListener("keypress", search)
+
+function getWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${apiKey}&units=metric`)
+    .then (request => request.json())
+    .then (function(data) {    })
+}
