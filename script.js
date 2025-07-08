e0f336d8df1d724fd4299b9a612337cc9acc5f8a
katerinahladikova <kat.hladikova@email.cz>
2025-07-08 18:16:56 +0200

    city filter

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 const apiKey = "fb3ca3d4a41c73fdf5013f2563d02016"
 const searchInput = document.getElementById("search")
+const searchList = document.getElementById("search-list")
+const selectedCity = document.getElementById("selected-city")
+let currentCity = null
 let cities = []
 
 
@@ -9,8 +12,25 @@ fetch("/city.list.json")
 .then (data => cities = data)
 
 searchInput.addEventListener("keypress", function() {
-    console.log(cities.filter(city => {
-        return city.name.toLowerCase().includes(searchInput.value.toLowerCase())
-    }))
+    const citiesFilter = 
+        cities.filter(city => {
+            return city.name.toLowerCase().includes(searchInput.value.toLowerCase())
+        })
+    
+    searchList.innerHTML = ""
+    citiesFilter.slice(0, 10).forEach(city => {
+        const cityEl = document.createElement("div")
+        const countryEl = document.createElement("span")
+        searchList.appendChild(cityEl)
+        cityEl.textContent = city.name
+        countryEl.textContent = city.country
+        cityEl.appendChild(countryEl)
+
+        cityEl.addEventListener("click", function(){
+            currentCity = city
+            selectedCity.textContent = city.name + ", " + city.country
+        })
+
+    })
 })
 
